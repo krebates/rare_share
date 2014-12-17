58a4458b325e9511b901cc4ec82edfa2d3759993
Krystle <krebates@gmail.com>
2014-12-16 23:34:32 -0500

    Community controller updates

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -8,4 +8,18 @@ class CommunitiesController < ApplicationController
     @community = Community.find(params[:id])
   end
 
+  def new
+    @community = Community.new
+    @user = current_user
+  end
+
+  # def create
+  #   @community = Community.new(community_params)
+  #   if @community.save
+  #     redirect_to api_applications_path, notice: t('flash.api_application.created')
+  #   else
+  #     redirect_to api_applications_path, alert: t('flash.api_application.unable_create')
+  #   end
+  # end
+
 end
