07586faf9e787fe9839ef654302ee15bf916f9ea
Jessica Simon <jsimon2789@gmail.com>
2015-01-25 16:46:31 -0500

    Show members in each community

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,4 +2,11 @@ class Community < ActiveRecord::Base
   has_many :memberships
   has_many :users, through: :memberships
   has_many :discussions
+
+  def members
+    memberships.map do |membership|
+      User.find(membership.user_id)
+    end
+  end
+
 end
