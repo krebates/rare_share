b79f55510cb35cbc069292a11fabd2c337e30e48
Krystle <krebates@gmail.com>
2015-01-26 22:06:13 -0500

    fixes search for communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,6 +2,7 @@ class Community < ActiveRecord::Base
   has_many :memberships
   has_many :users, through: :memberships
   has_many :discussions
+  searchkick
 
   def members
     memberships.map do |membership|
