3600dfc2d8993d638a2aed14f944542d70bfbe51
Jessica Simon <jsimon2789@gmail.com>
2015-02-12 20:08:51 -0500

    Authentication for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  USERS = { "user" => "secret" }
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_digest("Application") do |name|
+      USERS[name]
+    end
+  end
 end
