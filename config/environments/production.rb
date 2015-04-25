fc4335367b7d421be8e15ce7f46848f3d2f780cb
Jessica Simon <jsimon2789@gmail.com>
2015-04-25 12:55:18 -0400

    Add sendgrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  #
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }  #
  
   config.action_mailer.default_url_options = {:host => "rareshare.org"}
   # Code is not reloaded between requests.
